f7d8982584650220e62e96111b6e2080be50b8dd
ThePC <ThePC@DESKTOP-3MTRAIS>
2018-09-21 06:54:18 -0400

    hw

--- Rectangle-master/src/RectangleTest.java
+++ Rectangle-master/src/RectangleTest.java
@@ -6,11 +6,11 @@ public class RectangleTest {
 	{
 		//look in Rectangle API under constructors to find what Rectangle(5,10,20,30) does
 		Rectangle box = new Rectangle(5, 10, 20, 30);
-		
+		//sets box with x,y,width,height dimensions
 		
 		//TODO Move the rectangle (use a method from the API to move the rectangle
 		//                          by changing both the x and y coordinates)
-		
+		box.setLocation(6, 11);
 		//TODO Print the following information in the following format
 		/*
 		 * Expected new x value: ... (print what you expect the x value to be)
@@ -18,7 +18,10 @@ public class RectangleTest {
 		 * Expected new y value: ... (print what you expect the new y value to be)
 		 * Actual y value: ...  (use a method from the API to print the actual y value)
 		 */
-
+		//6
+		System.out.println(box.getX());
+		//11
+		System.out.print(box.getY());
 	}
 
 }
